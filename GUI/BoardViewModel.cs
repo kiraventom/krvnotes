350b5bc91ab2708a1b1d86727efc6e5233bb29b3
Alikhan Temirkanov <kiraventom@gmail.com>
2022-01-21 02:07:18 +0300

    Add possibility to add and delete notes

--- GUI/BoardViewModel.cs
+++ GUI/BoardViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Windows.Input;
+using GUI.Commands;
 
 namespace GUI;
 
@@ -8,13 +10,22 @@ public class BoardViewModel : INotifyPropertyChanged
     public BoardViewModel()
     {
         Notes = new ObservableCollection<NoteModel>();
+        CreateNoteCommand = new Command(CreateNewNote);
+        SaveNoteCommand = new Command<NoteModel>(SaveNote, note => note is not null);
+        DeleteNoteCommand = new Command<NoteModel>(DeleteNote, note => note is not null);
+        
         Notes.Add(new NoteModel() {Header = "Header", Text = "Text!!!"});
         Notes.Add(new NoteModel() {Header = "Кошка любимая", Text = "Я люблю её очень сильно мяу мур мяу!!!"});
     }
-    
+
+    private void DeleteNote(NoteModel note) => Notes.Remove(note);
+
     private NoteModel _currentNote;
 
+    public event PropertyChangedEventHandler PropertyChanged;
+
     public ObservableCollection<NoteModel> Notes { get; set; }
+
     public NoteModel CurrentNote
     {
         get => _currentNote;
@@ -24,8 +35,21 @@ public class BoardViewModel : INotifyPropertyChanged
             OnPropertyChanged(nameof(CurrentNote));
         }
     }
+    
+    public ICommand CreateNoteCommand { get; }
+    public ICommand SaveNoteCommand { get; }
+    public ICommand DeleteNoteCommand { get; }
 
-    public event PropertyChangedEventHandler PropertyChanged;
+    private void CreateNewNote()
+    {
+        CurrentNote = new NoteModel() {Header = "HeaderTest", Text = "TextTest"};
+    }
+
+    private void SaveNote(NoteModel note)
+    {
+        Notes.Add(note);
+        CurrentNote = null;
+    }
     
     protected virtual void OnPropertyChanged(string propertyName)
     {
---
+++ GUI/Commands/Command.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Input;
+
+namespace GUI.Commands;
+
+public class Command : ICommand
+{
+    public Command(Action action)
+    {
+        _action = action;
+    }
+
+    private readonly Action _action;
+    
+    public bool CanExecute(object parameter) => true;
+
+    public void Execute(object parameter) => _action.Invoke();
+
+    public event EventHandler CanExecuteChanged;
+}
+
+public class Command<T> : ICommand
+{
+    public Command(Action<T> action, Predicate<T> predicate)
+    {
+        _action = action;
+        _predicate = predicate;
+    }
+
+    private readonly Action<T> _action;
+    private readonly Predicate<T> _predicate;
+    
+    public bool CanExecute(object parameter)
+    {
+        if (parameter is T t)
+            return CanExecute(t);
+        
+        return false;
+    }
+    
+    public bool CanExecute(T parameter)
+    {
+        return _predicate.Invoke(parameter);
+    }
+
+    public void Execute(object parameter)
+    {
+        if (parameter is T t)
+            Execute(t);
+        else
+            throw new ArgumentException();
+    }
+
+    public void Execute(T parameter)
+    {
+        _action.Invoke(parameter);
+    }
+    
+    public event EventHandler CanExecuteChanged
+    {
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
+    }
+
+    public void RaiseCanExecuteChanged()
+    {
+        CommandManager.InvalidateRequerySuggested();
+    }
+}
