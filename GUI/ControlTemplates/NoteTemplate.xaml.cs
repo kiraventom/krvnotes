1e3f2f5c3580ef9b1a1bacdf4889a7b6e24ee7a1
Alikhan Temirkanov <kiraventom@gmail.com>
2022-02-27 17:06:11 +0300

    Rewrite controls as templates

---
+++ GUI/ControlTemplates/NoteTemplate.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace GUI.ControlTemplates;
+
+public class NoteTemplate : Control
+{
+    public static readonly DependencyProperty HeaderProperty =
+        DependencyProperty.Register(
+            nameof(Header), 
+            typeof(string), 
+            typeof(NoteTemplate),
+            new PropertyMetadata(string.Empty));
+
+    public static readonly DependencyProperty TextProperty =
+        DependencyProperty.Register(
+            nameof(Text), 
+            typeof(string), 
+            typeof(NoteTemplate),
+            new PropertyMetadata(string.Empty));
+
+    public static readonly DependencyProperty OpenNoteCommandProperty =
+        DependencyProperty.Register(
+            nameof(OpenNoteCommand), 
+            typeof(ICommand), 
+            typeof(NoteTemplate));
+    
+    public static readonly DependencyProperty OpenNoteCommandParameterProperty =
+        DependencyProperty.Register(
+            nameof(OpenNoteCommandParameter), 
+            typeof(object), 
+            typeof(NoteTemplate));
+    
+    public static readonly DependencyProperty DeleteNoteCommandProperty =
+        DependencyProperty.Register(
+            nameof(DeleteNoteCommand), 
+            typeof(ICommand), 
+            typeof(NoteTemplate));
+    
+    public static readonly DependencyProperty DeleteNoteCommandParameterProperty =
+        DependencyProperty.Register(
+            nameof(DeleteNoteCommandParameter), 
+            typeof(object), 
+            typeof(NoteTemplate));
+
+    public string Header
+    {
+        get => (string)GetValue(HeaderProperty);
+        set => SetValue(HeaderProperty, value);
+    }
+
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+    
+    public ICommand OpenNoteCommand
+    {
+        get => (ICommand)GetValue(OpenNoteCommandProperty);
+        set => SetValue(OpenNoteCommandProperty, value);
+    }
+    
+    public object OpenNoteCommandParameter
+    {
+        get => GetValue(OpenNoteCommandParameterProperty);
+        set => SetValue(OpenNoteCommandParameterProperty, value);
+    }
+
+    public ICommand DeleteNoteCommand
+    {
+        get => (ICommand)GetValue(DeleteNoteCommandProperty);
+        set => SetValue(DeleteNoteCommandProperty, value);
+    }
+    
+    public object DeleteNoteCommandParameter
+    {
+        get => GetValue(DeleteNoteCommandParameterProperty);
+        set => SetValue(DeleteNoteCommandParameterProperty, value);
+    }
+}
---
+++ GUI/ControlTemplates/PlaceholderTemplate.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace GUI.ControlTemplates;
+
+public class PlaceholderTextBox : Control
+{
+    public static readonly DependencyProperty TextProperty =
+        DependencyProperty.Register(
+            nameof(Text), 
+            typeof(string), 
+            typeof(PlaceholderTextBox),
+            new PropertyMetadata(string.Empty));
+    
+    public static readonly DependencyProperty PlaceholderProperty =
+        DependencyProperty.Register(
+            nameof(Placeholder), 
+            typeof(string), 
+            typeof(PlaceholderTextBox),
+            new PropertyMetadata(string.Empty));
+
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+    public string Placeholder
+    {
+        get => (string)GetValue(PlaceholderProperty);
+        set => SetValue(PlaceholderProperty, value);
+    }
+}
+
+[ValueConversion(typeof(int), typeof(Visibility))]
+public class LengthToVisibilityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        int length = (int) value;
+        return length == 0 ? Visibility.Visible : Visibility.Hidden;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return DependencyProperty.UnsetValue;
+    }
+}
