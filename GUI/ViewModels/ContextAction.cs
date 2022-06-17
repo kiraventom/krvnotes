e0082690cdfb9d4537995b10c9250cc47bfdc4e4
Alikhan Temirkanov <kiraventom@gmail.com>
2022-06-18 02:44:23 +0300

    Menu WIP

---
+++ GUI/ViewModels/ContextAction.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Input;
+
+namespace GUI.ViewModels
+{
+    internal class ContextAction
+    {
+        public string Text { get; }
+        public ICommand Command { get; }
+    }
+}
